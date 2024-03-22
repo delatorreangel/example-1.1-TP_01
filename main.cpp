c531d390fcc21e8e84e35dbd84d8e9d257c1df02
delatorreangel <adelatorre@fi.uba.ar>
2024-03-22 00:27:34 +0000

    ejemplo 1.1 modificado

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
+// Grupo de placa 2824
+// Integrantes 
+        
+        // Zambrana Erwin
+        // de la Torre Angel
+
+
 #include "mbed.h"
 #include "arm_book_lib.h"
+#include <cstdio>
 
 int main()
-{
-    DigitalIn gasDetector(D2);
-
+{  
+    // Se crea un objeto llamado gasDetector() de clase DigitalIn, que inicializa el pind D2 como entrada.
+    DigitalIn gasDetector(D2);   
+    
+     // Se crea un objeto llamado alarmLed() de clase DigitalOut, que inicializa el LED1 como salida
+     // en el que esta conectado el led1 que está integrado en la placa nucleo.
     DigitalOut alarmLed(LED1);
-
+    
+    //  Se habilita la resistencia PullDown en el Objeto gasDetector.
     gasDetector.mode(PullDown);
-
+    
+    // se inicializa la variable alarmLed como apagado.
     alarmLed = OFF;
 
-    while (true) {
+    while (true) { 
+        printf("%s\n","Hola mundo!");  // Escribimos Hola mundo en el puerto Com3, con 9600 br/s
         if ( gasDetector == ON ) {
             alarmLed = ON;
         }
